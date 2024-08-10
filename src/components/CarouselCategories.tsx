5a12aa1926bbd1e02aa031ba9d8a380aef0303db
MarceloHenrique7 <marcelohenriquex77@gmail.com>
2024-08-10 14:10:21 -0300

    att carousel

--- src/components/CarouselCategories.tsx
+++ src/components/CarouselCategories.tsx
@@ -8,16 +8,16 @@ import objectCategories from '../config/categories-config'
 
 const CarouselCategories = () => {
     return (
-        <Carousel className="max-w-[80%] md:w-full">
+        <Carousel className="w-full md:w-full">
             <CarouselContent className="font-medium text-center">
             {
                 objectCategories.map((item) => (
-                    <CarouselItem key={item.label} className="basis-1/3 md:basis-1/5 flex flex-col items-center">
-                        <Link to={`/search/recipe/category/${item.label.toLowerCase()}`} className="flex flex-col items-center justify-center">
-                            <span className="w-[80px] h-[80px]">
+                    <CarouselItem key={item.label} className="md:basis-1/3 flex flex-col items-center">
+                        <Link to={`/search/recipe/category/${item.label.toLowerCase()}`} className="flex flex-col items-center justify-center gap-2">
+                            <span className="w-[180px] h-[180px]">
                                 <img src={item.src} alt={item.label} className="object-cover w-full h-full rounded-full"/>
                             </span>
-                            <span>
+                            <span className="font-bold text-2xl">
                                 {item.label}
                             </span>
                         </Link>
@@ -25,8 +25,6 @@ const CarouselCategories = () => {
                 ))
             }
         </CarouselContent>
-        <CarouselPrevious />
-        <CarouselNext />
     </Carousel>
     )
 }
