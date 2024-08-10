62ea1bc06889b9e94f8901fc6980d6fd947a2293
MarceloHenrique7 <marcelohenriquex77@gmail.com>
2024-08-10 14:36:55 -0300

    test

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -52,10 +52,6 @@ const HomePage = () => {
                 </span>
                 <SearchBar placeHolder="Search by Recipe" onSubmit={handleSearchSubmit} />
             </div>
-            <div className="flex flex-col items-center gap-8">
-                <h1 className="font-bold text-3xl">Search By Category</h1>
-                <CarouselCategories />
-            </div>
             <TabsList className="w-full flex items-center justify-between">
                 <TabsTrigger value="free-recipes" className="w-full">
                     Recipes Free
