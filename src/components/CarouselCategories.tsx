f39d052d28fe34d3f28ea9cc0ba92913f0ff87c5
MarceloHenrique7 <marcelohenriquex77@gmail.com>
2024-08-10 14:25:25 -0300

    at

--- src/components/CarouselCategories.tsx
+++ src/components/CarouselCategories.tsx
@@ -1,7 +1,7 @@
 
 
 
-import { Carousel, CarouselContent, CarouselItem } from "@/components/ui/carousel";
+import { Carousel, CarouselContent } from "@/components/ui/carousel";
 import { Link } from "react-router-dom";
 
 import objectCategories from '../config/categories-config'
@@ -9,19 +9,18 @@ import objectCategories from '../config/categories-config'
 const CarouselCategories = () => {
     return (
         <Carousel className="w-full md:w-full">
-            <CarouselContent className="font-medium text-center">
+            <CarouselContent className="font-medium text-center flex gap-2">
             {
                 objectCategories.map((item) => (
-                        <Link to={`/search/recipe/category/${item.label.toLowerCase()}`} className="flex flex-col items-center justify-center gap-2">
-                        <CarouselItem key={item.label} className="md:basis-1/3 flex flex-col items-center">
+                        <Link to={`/search/recipe/category/${item.label.toLowerCase()}`} className="flex flex-col items-center justify-center gap-2" >
                             <span className="w-[80px] h-[80px]">
                                 <img src={item.src} alt={item.label} className="object-cover w-full h-full rounded-full"/>
                             </span>
                             <span className="font-bold text-sm">
                                 {item.label}
                             </span>
-                        </CarouselItem>
                         </Link>
+
                 ))
             }
         </CarouselContent>
